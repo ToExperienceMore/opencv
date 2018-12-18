a0039037b7753fcfcf23ed3a19adeb1cbc9bcfab
ToExpM <995776046@qq.com>
2018-12-18 14:58:29 +0800

    add a test file

---
+++ test.c
@@ -0,0 +1,22 @@
+/**
+ * @file test.c
+ * @brief
+ * @author lingmm
+ * @version 1.0
+ * @date 2018-12-18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+
+	printf("opencv!");
+
+
+	return 0;
+}
+
+
