a278a5e0f0e87d800f5cea43327bfacfa62cb253
ToExpM <995776046@qq.com>
2018-12-18 15:52:27 +0800

    add hello.c

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * @file hello.c
+ * @brief
+ * @author lingmm
+ * @version 1.0
+ * @date 2018-12-18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	
+	printf("hello!\n");
+
+	return 0;
+}
+
+
